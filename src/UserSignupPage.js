d78f06ec1cc342a789ce00c661b495f8d77fd926
atasen19 <atasen19@itu.edu.tr>
2023-03-25 01:58:25 +0300

    wip: axios proxy does not work

--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -1,43 +1,14 @@
 import React from "react";
+import axios from "axios";
 
 class UserSignupPage extends React.Component{
 
     state = {
         username: null,
-        agreedClicked: false,
+        displayname: null,
         password: null,
         passwordRepeat: null
     }
-
-    // onChangeUserName = (event) => {
-    //     this.setState({
-    //         username: event.target.value
-    //     })
-    // }
-
-    // onChangeDisplayName = (event) => {
-    //     this.setState({
-    //         displayName: event.target.value
-    //     })
-    // }
-
-    // onChangeAgree = (event) => {
-    //     this.setState({
-    //         agreedClicked: event.target.checked
-    //     })
-    // }
-
-    // onChangePassword = (event) => {
-    //     this.setState({
-    //         password: event.target.value
-    //     })
-    // }
-
-    // onChangePasswordRepeat = (event) => {
-    //     this.setState({
-    //         passwordRepeat: event.target.value
-    //     })
-    // }
 
     onChange = event => {
         const { name, value } = event.target
@@ -45,6 +16,19 @@ class UserSignupPage extends React.Component{
         this.setState({
             [name]: value
         })
+    }
+
+    onClickSignup = event => {
+        event.preventDefault();
+
+        const {username, displayname, password} = this.state;
+        const body = {
+            username,
+            displayname,
+            password
+        }
+
+        axios.post('/api/v1/users', body)
     }
 
     render(){
@@ -58,7 +42,7 @@ class UserSignupPage extends React.Component{
                 </div>
                 <div>
                     <label>Display Name</label>
-                    <input name="displayName" onChange={this.onChange}>
+                    <input name="displayname" onChange={this.onChange}>
                     </input>
                 </div>
                 <div>
@@ -72,11 +56,7 @@ class UserSignupPage extends React.Component{
                     </input>
                 </div>
                 <div>
-                    <label>Agreed</label>
-                    <input type="checkbox" onChange={this.onChange}></input>
-                </div>
-                <div>
-                    <button disabled={!this.state.agreedClicked} >Sign Up</button>
+                    <button onClick={this.onClickSignup} >Sign Up</button>
                 </div>
             </form>
         )
